d20bfc77323d8f67455e931772a8ae352f0e7bd4
rreyes@wesleyan.edu <rreyes@wesleyan.edu>
2024-12-01 10:50:48 -0500

    Initial commit

--- R/ryx fcn.R
+++ R/ryx fcn.R
@@ -4,6 +4,7 @@
 #'  variables to include in the comparison. If no numeric variable is specified
 #'  the function will compare the selected variable with all of the other variables
 #'  in the data set. The function also evaluates the statistical significance of each correlation.
+#' @export
 #' @param data A data frame containing the dataset to be analyzed.
 #' @param y A character string specifying the variable (must be numeric) for correlation analysis.
 #' @param x An optional character string specifying the variable that the user wants to compare variable y to.
@@ -20,7 +21,7 @@
 #' # Example usage with the mtcars dataset
 #' library(datasets)
 #' object2 <- ryx(mtcars, y = "mpg")
-#' @export
+
 ryx <- function(data, y, x){
   if(missing(x)){
     x <- names(data)[sapply(data, class)=="numeric"]
